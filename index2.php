12532b709b3ce2404661ddfe000226368fc54cab
ducbl98 <ductit1998@gmail.com>
2021-06-28 13:42:09 +0700

    Demo JQuery

---
+++ index2.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo JQuery</title>
+    <link rel="stylesheet" href="css/index2.css" type="text/css">
+</head>
+<body>
+<div id="myDIV" class="header">
+    <h2 style="margin: 5px">My To Do List</h2>
+    <input type="text" id="myInput" placeholder="Title ...">
+    <span class="addBtn" onclick="newElement()">Add</span>
+</div>
+<ul id="myUL">
+    <li>Hit the gym</li>
+    <li>Pay bills</li>
+    <li>Meet George</li>
+    <li>Buy Eggs</li>
+    <li>Read A Book</li>
+    <li>Organize office</li>
+</ul>
+<script src="js/index2.js"></script>
+</body>
+</html>
